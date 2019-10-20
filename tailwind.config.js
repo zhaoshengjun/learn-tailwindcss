b28bcbbebd9e7cf4f45628ac76c363fa58d00706
Sam Zhao <zhao.shengjun@gmail.com>
2019-10-20 21:20:59 +1000

    add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"brand-blue": "#1992d4"
+			}
+		}
 	},
 	variants: {
 		backgroundColor: ["responsive", "hover", "focus", "active"] // order matters!!!
